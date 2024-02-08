817ab4324228e3921c3237446edd5c41d6b64660
unknown <unekue2@gmail.com>
2024-02-08 14:30:31 +0100

    sorry

--- db/db_setup.py
+++ db/db_setup.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # set up db using progress
-SQLALCHEMY_DATABASE_URL = "postgresql://uneku:pgadmin@35.176.33.2:5432/fastapilmsdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://<user>:<pwd>@host_ip:5432/fastapilmsdb"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
